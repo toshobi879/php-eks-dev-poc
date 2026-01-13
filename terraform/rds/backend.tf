b39e000f9a0d64fc7a8478b6379faae186aff02e
toshobi879 <toshobi879@gmail.com>
2026-01-13 17:55:31 +0530

    Delete terraform/rds/backend.tf

--- terraform/rds/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "caltrans-eks-terraform-state-bucket"
-    key            = "rds/php-poc/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "caltrans-eks-terraform-lock"
-    encrypt        = true
-  }
-}
