76cd388b8ca51ef1f2c54c4ee7c6aa1a4b82fbd8
toshobi879 <toshobi879@gmail.com>
2026-01-13 17:52:22 +0530

    Update backend.tf

--- terraform/eks/backend.tf
+++ terraform/eks/backend.tf
@@ -4,6 +4,5 @@ terraform {
     key            = "eks/php-poc/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "caltrans-eks-terraform-lock"
-    encrypt        = true
   }
 }
