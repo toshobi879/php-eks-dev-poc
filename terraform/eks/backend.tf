af071254fe27592d0e3c7d55da7d2caa4ec998e3
toshobi879 <toshobi879@gmail.com>
2026-01-13 17:10:32 +0530

    Create backend.tf

---
+++ terraform/eks/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "caltrans-eks-terraform-state-bucket"
+    key            = "eks/php-poc/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "caltrans-eks-terraform-lock"
+    encrypt        = true
+  }
+}
