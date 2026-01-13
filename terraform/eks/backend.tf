e95e3fddba4580fe8a14d563b37595597b12c55c
toshobi879 <toshobi879@gmail.com>
2026-01-13 17:54:48 +0530

    Delete terraform/eks/backend.tf

--- terraform/eks/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "caltrans-eks-terraform-state-bucket"
-    key            = "eks/php-poc/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "caltrans-eks-terraform-lock"
-  }
-}
