a38b442f83fba834b9ce655f8a453c76e7e375ba
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:24:40 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,85 +1,23 @@
-############################################
-# VPC for EKS (using official VPC module)
-############################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = "${var.project_name}-vpc"
-  cidr = "10.20.0.0/16"
-
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
-  public_subnets  = ["10.20.11.0/24", "10.20.12.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Project     = var.project_name
-    Environment = "poc"
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "billow-poc-terraform-state"
+    key    = "vpc/terraform.tfstate"
+    region = "us-west-2"
   }
 }
 
-############################################
-# EKS Cluster (using official EKS module)
-############################################
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.11"
 
   cluster_name    = "${var.project_name}-cluster"
   cluster_version = var.eks_cluster_version
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 
   cluster_endpoint_public_access = true
-
-  ############################################
-  # ✅ AUTOMATED EKS ACCESS (NO aws-auth)
-  ############################################
-  access_entries = {
-
-    ##########################################
-    # SSO Admin (Human access)
-    ##########################################
-    eks_admin_sso = {
-      principal_arn = "arn:aws:iam::020408743573:role/aws-reserved/sso.amazonaws.com/us-west-2/AWSReservedSSO_AWSAdministratorAccess_3fed78f1fb50999a"
-
-      policy_associations = {
-        cluster_admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-
-    ##########################################
-    # GitHub Actions (CI/CD automation)
-    ##########################################
-    github_actions = {
-      principal_arn = "arn:aws:iam::020408743573:role/php-eks-dev-poc-role"
-
-      policy_associations = {
-        cluster_admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
-
-  ############################################
 
   eks_managed_node_groups = {
     default = {
@@ -87,20 +25,11 @@ module "eks" {
       max_size       = var.desired_capacity + 1
       min_size       = 1
       instance_types = [var.node_instance_type]
-
-      tags = {
-        Name = "${var.project_name}-node"
-      }
     }
-  }
-
-  cluster_tags = {
-    Project     = var.project_name
-    Environment = "poc"
   }
 
   tags = {
     Project     = var.project_name
-    Environment = "poc"
+    Environment = var.environment
   }
 }
