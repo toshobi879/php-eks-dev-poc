6a7a6dc4e6370ea6fc1c7a255b1d07141f8204f7
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:30:25 +0530

    Create variables.tf

---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
