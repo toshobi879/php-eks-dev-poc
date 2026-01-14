acdf64ef680957f72130d32ccf2afa9ae6fcd73e
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:36:49 +0530

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,11 +1,14 @@
 variable "aws_region" {
-  type = string
+  description = "AWS region where backend resources will be created"
+  type        = string
 }
 
 variable "backend_bucket_name" {
-  type = string
+  description = "Name of the S3 bucket to store Terraform state"
+  type        = string
 }
 
 variable "dynamodb_table_name" {
-  type = string
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
 }
