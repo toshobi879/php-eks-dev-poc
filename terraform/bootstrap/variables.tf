f0d6bb8b2c7e6f15581249501f378bed45b37bb4
toshobi879 <toshobi879@gmail.com>
2026-01-14 18:49:57 +0530

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,20 +1,16 @@
 variable "aws_region" {
-  description = "AWS region where backend resources will be created"
-  type        = string
+  type = string
 }
 
 variable "backend_bucket_name" {
-  description = "Name of the S3 bucket to store Terraform state"
-  type        = string
+  type = string
 }
 
 variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for Terraform state locking"
-  type        = string
+  type = string
 }
 
 variable "github_actions_role_name" {
   description = "Name of the IAM role used by GitHub Actions (OIDC)"
   type        = string
 }
-
