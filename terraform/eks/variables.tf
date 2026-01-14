4baaf5a903e644d8025c9c765c10d09dd9613e9c
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:25:47 +0530

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,24 +1,23 @@
 variable "aws_region" {
-  description = "AWS region to deploy EKS"
-  type        = string
+  type = string
 }
 
 variable "project_name" {
-  description = "Prefix for all resources"
-  type        = string
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
 
 variable "eks_cluster_version" {
-  description = "EKS Kubernetes version"
-  type        = string
+  type = string
 }
 
 variable "node_instance_type" {
-  description = "Instance type for worker nodes"
-  type        = string
+  type = string
 }
 
 variable "desired_capacity" {
-  description = "Desired number of worker nodes"
-  type        = number
+  type = number
 }
