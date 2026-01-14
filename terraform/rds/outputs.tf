e0bfa778a0811bf3c27ac7231a6af63ee4f649a3
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:32:15 +0530

    Update outputs.tf

--- terraform/rds/outputs.tf
+++ terraform/rds/outputs.tf
@@ -1,4 +1,7 @@
-output "rds_endpoint" { value = aws_db_instance.mysql.address }
-output "rds_port"     { value = aws_db_instance.mysql.port }
-output "db_name"      { value = var.db_name }
-output "db_username"  { value = var.db_username }
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.address
+}
+
+output "rds_port" {
+  value = aws_db_instance.mysql.port
+}
