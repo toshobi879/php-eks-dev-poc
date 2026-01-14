3b6d57fd48b470147a8e3ca445b836c1d02d228f
toshobi879 <toshobi879@gmail.com>
2026-01-14 17:25:12 +0530

    Update outputs.tf

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,19 +1,7 @@
 output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_region" {
-  description = "AWS region"
-  value       = var.aws_region
-}
-
-output "cluster_oidc_issuer_url" {
-  description = "OIDC issuer URL"
-  value       = module.eks.cluster_oidc_issuer_url
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
 }
