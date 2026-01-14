db8ca7462b5be1166f185d2c754b1fa391050129
toshobi879 <toshobi879@gmail.com>
2026-01-14 19:49:47 +0530

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -18,3 +18,19 @@ variable "node_instance_type" {
 variable "desired_capacity" {
   type = number
 }
+#############
+variable "github_actions_role_arn" {
+  description = "IAM role ARN used by GitHub Actions"
+  type        = string
+}
+
+variable "sso_admin_role_arn" {
+  description = "SSO admin role ARN for human access"
+  type        = string
+}
+
+variable "eks_admin_policy_arn" {
+  description = "EKS cluster admin policy ARN"
+  type        = string
+  default     = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
