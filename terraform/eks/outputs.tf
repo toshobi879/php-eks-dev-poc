3bf9e38a25c8b1f336f5a49d93748b43c185ccbf
toshobi879 <toshobi879@gmail.com>
2026-01-15 16:09:48 +0530

    Update outputs.tf

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -9,12 +9,12 @@ output "cluster_endpoint" {
 }
 
 output "cluster_security_group_id" {
-  description = "EKS cluster security group ID (control plane)"
+  description = "EKS cluster security group ID"
   value       = module.eks.cluster_security_group_id
 }
 
 output "node_security_group_id" {
-  description = "EKS worker node security group ID (used for RDS access)"
+  description = "EKS worker node security group ID )"
   value       = module.eks.node_security_group_id
 }
 
