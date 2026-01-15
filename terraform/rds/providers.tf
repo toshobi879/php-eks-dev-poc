83cd8d65dadac68ac332d886adf5d682c8815300
toshobi879 <toshobi879@gmail.com>
2026-01-15 16:17:30 +0530

    Update providers.tf

--- terraform/rds/providers.tf
+++ terraform/rds/providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.7.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.30"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
