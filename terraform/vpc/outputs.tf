20ca776e7f00289303974f8084d14d58698e8f89
toshobi879 <toshobi879@gmail.com>
2026-01-15 16:30:18 +0530

    Update outputs.tf

--- terraform/vpc/outputs.tf
+++ terraform/vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "VPC ID for EKS and RDS"
+  value       = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  description = "Private subnets"
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  description = "Public subnets"
+  value       = module.vpc.public_subnets
 }
